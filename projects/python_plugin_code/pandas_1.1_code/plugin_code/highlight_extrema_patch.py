5f7c61457c547de6fcb926d974857451d0a8f8ea
Daniel Schmidt <rendner@users.noreply.github.com>
2021-12-01 12:04:02 +0100

    add license headers

--- projects/python_plugin_code/pandas_1.1_code/plugin_code/highlight_extrema_patch.py
+++ projects/python_plugin_code/pandas_1.1_code/plugin_code/highlight_extrema_patch.py
@@ -1,3 +1,17 @@
+#  Copyright 2021 cms.rendner (Daniel Schmidt)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 # source "_highlight_handler": https://github.com/pandas-dev/pandas/blob/v1.1.5/pandas/io/formats/style.py#L1386-L1397
 # source "_highlight_extrema": https://github.com/pandas-dev/pandas/blob/v1.1.5/pandas/io/formats/style.py#L1400-L1418
 
--- projects/python_plugin_code/pandas_1.2_code/plugin_code/background_gradient_patch.py
+++ projects/python_plugin_code/pandas_1.2_code/plugin_code/background_gradient_patch.py
@@ -1,3 +1,17 @@
+#  Copyright 2021 cms.rendner (Daniel Schmidt)
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 # source "background_gradient": https://github.com/pandas-dev/pandas/blob/v1.2.5/pandas/io/formats/style.py#L1162-L1241
 # source "_background_gradient": https://github.com/pandas-dev/pandas/blob/v1.2.5/pandas/io/formats/style.py#L1244-L1307
 from plugin_code.apply_args import ApplyArgs
