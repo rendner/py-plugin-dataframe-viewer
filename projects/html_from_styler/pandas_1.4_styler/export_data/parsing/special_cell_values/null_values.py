345b9cb2cbef2bff43e9d5ea690c1c0c57c68a45
Daniel Schmidt <rendner@users.noreply.github.com>
2022-01-06 15:23:45 +0100

    [styler-projects] remove obsolete pandas version check

--- projects/html_from_styler/pandas_1.4_styler/export_data/parsing/special_cell_values/null_values.py
+++ projects/html_from_styler/pandas_1.4_styler/export_data/parsing/special_cell_values/null_values.py
@@ -18,8 +18,5 @@ df = pd.DataFrame({"a": [pd.NA, 1, None], "b": [np.nan, 1, -1]})
 
 test_case = {
     "styler": df.style.format(na_rep='').highlight_min(),
-    "chunk_size": (1, 2),
-    # in pandas < 1.3.2 there was a bug: https://github.com/pandas-dev/pandas/pull/42650
-    # which raised an error when calling "df.style.highlight_min().render()"
-    "pandas_version": ">=1.3.2"
+    "chunk_size": (1, 2)
 }
