c9e6a9fdd7df066d32f55ee22f1611b367858c3b
Daniel Schmidt <rendner@users.noreply.github.com>
2022-01-28 23:18:52 +0100

    [styler-projects] fix non seeded-random

--- projects/html_from_styler/pandas_1.4_styler/export_data/parsing/headers/hide_specific_level.py
+++ projects/html_from_styler/pandas_1.4_styler/export_data/parsing/headers/hide_specific_level.py
@@ -14,6 +14,8 @@
 import numpy as np
 from pandas import MultiIndex, DataFrame
 
+np.random.seed(123456)
+
 midx = MultiIndex.from_product([["x", "y"], ["a", "b", "c"]])
 df = DataFrame(np.random.randn(6, 6), index=midx, columns=midx)
 
