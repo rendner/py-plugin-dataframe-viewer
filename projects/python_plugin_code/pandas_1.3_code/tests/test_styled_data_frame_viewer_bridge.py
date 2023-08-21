383877b9fee5687a40d615f8755164fb79fc1248
Daniel Schmidt <rendner@users.noreply.github.com>
2023-08-21 23:17:42 +0200

    [code-projects] pandas 1.3 - allow to create a DataFrame from a dictionary

--- projects/python_plugin_code/pandas_1.3_code/tests/test_styled_data_frame_viewer_bridge.py
+++ projects/python_plugin_code/pandas_1.3_code/tests/test_styled_data_frame_viewer_bridge.py
@@ -1,4 +1,4 @@
-#  Copyright 2021 cms.rendner (Daniel Schmidt)
+#  Copyright 2023 cms.rendner (Daniel Schmidt)
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
@@ -12,14 +12,19 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 import pandas as pd
+import pytest
 
+from plugin_code.create_fingerprint import create_fingerprint
 from plugin_code.patched_styler import PatchedStyler
-from plugin_code.styled_data_frame_viewer_bridge import StyledDataFrameViewerBridge
+from plugin_code.styled_data_frame_viewer_bridge import \
+    StyledDataFrameViewerBridge, CreatePatchedStylerConfig, CreatePatchedStylerFailure
 
-df = pd.DataFrame.from_dict({
+df_dict = {
     "col_0": [4, 4, 4, 1, 4],
     "col_1": [1, 4, 4, 1, 2],
-})
+}
+
+df = pd.DataFrame.from_dict(df_dict)
 
 
 def test_create_patched_styler_for_df():
@@ -30,35 +35,72 @@ def test_create_patched_styler_for_styler():
     assert isinstance(StyledDataFrameViewerBridge.create_patched_styler(df.style), PatchedStyler)
 
 
-def test_create_fingerprint_two_different_styler_but_same_df_have_same_fingerprint():
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(df.style.highlight_max())
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(df.style.highlight_min())
-    assert f1 == f2
-
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(df.style)
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(df.style)
-    assert f1 == f2
+def test_create_patched_styler_for_dict_orient_columns():
+    ps = StyledDataFrameViewerBridge.create_patched_styler(df_dict)
+    assert isinstance(ps, PatchedStyler)
+    assert list(ps.internal_get_context().get_visible_frame().columns) == list(df_dict.keys())
 
 
-def test_create_fingerprint_same_df_have_same_fingerprint():
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(df)
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(df)
-    assert f1 == f2
+def test_create_patched_styler_for_dict_orient_index():
+    ps = StyledDataFrameViewerBridge.create_patched_styler(
+        df_dict,
+        CreatePatchedStylerConfig(data_source_to_frame_hint="DictKeysAsRows"),
+    )
+    assert isinstance(ps, PatchedStyler)
+    assert list(ps.internal_get_context().get_visible_frame().columns) == [0, 1, 2, 3, 4]
 
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(df)
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(df.style.data)
-    assert f1 == f2
 
 
-def test_create_fingerprint_different_df_have_different_fingerprint():
-    a = pd.DataFrame.from_dict({"A": [1]})
-    b = pd.DataFrame.from_dict({"A": [1]})
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(a)
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(b)
-    assert f1 != f2
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.from_dict.html
+# the passed dict has to be of the form {field : array-like} or {field : dict},
+# otherwise an error is raised
+# https://github.com/pandas-dev/pandas/issues/12387
+def test_create_patched_styler_for_dict_with_scalars_raises_error_as_expected():
+    with pytest.raises(ValueError, match="If using all scalar values, you must pass an index"):
+        StyledDataFrameViewerBridge.create_patched_styler({"a": 1})
 
-    a = pd.DataFrame.from_dict({"A": [1]})
-    b = pd.DataFrame.from_dict({"A": [1]}).style
-    f1 = StyledDataFrameViewerBridge.create_fingerprint(a)
-    f2 = StyledDataFrameViewerBridge.create_fingerprint(b)
-    assert f1 != f2
+
+def test_create_patched_styler_for_dict_keys_as_rows():
+    ps = StyledDataFrameViewerBridge.create_patched_styler(
+        df_dict,
+        CreatePatchedStylerConfig(data_source_to_frame_hint="DictKeysAsRows"),
+    )
+    assert isinstance(ps, PatchedStyler)
+    assert list(ps.internal_get_context().get_visible_frame().index) == list(df_dict.keys())
+
+
+def test_create_patched_styler_fails_on_unsupported_data_source():
+    assert StyledDataFrameViewerBridge.create_patched_styler([1]) == CreatePatchedStylerFailure(
+        error_kind="UNSUPPORTED_DATA_SOURCE_TYPE",
+        info="<class 'list'>",
+    ).to_json()
+
+
+def test_create_patched_styler_fails_on_invalid_fingerprint():
+    assert StyledDataFrameViewerBridge.create_patched_styler(
+        df,
+        CreatePatchedStylerConfig(previous_fingerprint=""),
+    ) == CreatePatchedStylerFailure(
+        error_kind="INVALID_FINGERPRINT",
+        info=create_fingerprint(df),
+    ).to_json()
+
+
+def test_create_patched_styler_fails_on_failing_eval_filter():
+    assert StyledDataFrameViewerBridge.create_patched_styler(
+        df,
+        CreatePatchedStylerConfig(filter_eval_expr="xyz"),
+    ) == CreatePatchedStylerFailure(
+        error_kind="FILTER_FRAME_EVAL_FAILED",
+        info="NameError(\"name 'xyz' is not defined\")",
+    ).to_json()
+
+
+def test_create_patched_styler_fails_on_wrong_filter_type():
+    assert StyledDataFrameViewerBridge.create_patched_styler(
+        df,
+        CreatePatchedStylerConfig(filter_eval_expr="df.style"),
+    ) == CreatePatchedStylerFailure(
+        error_kind="FILTER_FRAME_OF_WRONG_TYPE",
+        info="<class 'pandas.io.formats.style.Styler'>",
+    ).to_json()
